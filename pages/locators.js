af0a3216d2d340f8b58366e09a6232353431835e
Anchal Soni <anchalsoni1991@gmail.com>
2022-02-06 17:55:58 +0530

    added const in place of module.export

--- pages/locators.js
+++ pages/locators.js
@@ -1,11 +1,5 @@
-module.exports = {
   //UI locators and city's name and url
-  accuweatherSearchBar: '//div[@class="searchbar-content"]//input[@class="search-input"]',
-  accuweatherTemperature:"//div[@class='forecast-container']//div[@class='temp']",
-  getTemperature:"//div[@class='forecast-container']//div[@class='temp']",
- // jammuCity:'Jammu,Jammu and Kashmir,IN',
-  //bengaluruCity:'bengaluru,Karnataka,IN',
-  //accuweatherBengaluruCityLandingPage: "https://www.accuweather.com/en/in/bengaluru",
-  //accuweatherJammuCityLandingPage:"https://www.accuweather.com/en/in/jammu",
-  //enterKey:'Enter'
-}
+  const  accuweatherSearchBar = '//div[@class="searchbar-content"]//input[@class="search-input"]',
+  const accuweatherTemperature ="//div[@class='forecast-container']//div[@class='temp']",
+  const getTemperature = "//div[@class='forecast-container']//div[@class='temp']"
+
