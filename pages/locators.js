6ad4c4bc726032ba03c7b021a9d0e73461bbda47
Anchal Soni <anchalsoni1991@gmail.com>
2022-02-06 18:15:46 +0530

    trying without module.export

--- pages/locators.js
+++ pages/locators.js
@@ -1,5 +1,5 @@
   //UI locators and city's name and url
-  const  accuweatherSearchBar = '//div[@class="searchbar-content"]//input[@class="search-input"]',
-  const accuweatherTemperature ="//div[@class='forecast-container']//div[@class='temp']",
-  const getTemperature = "//div[@class='forecast-container']//div[@class='temp']"
+ const accuweatherSearchBar = '//div[@class="searchbar-content"]//input[@class="search-input"]';
+ const accuweatherTemperature ="//div[@class='forecast-container']//div[@class='temp']";
+ const getTemperature = "//div[@class='forecast-container']//div[@class='temp']"
 
