3653a860a62460fc52e94cf7ed05ff2f8a1adcea
Anchal Soni <anchalsoni1991@gmail.com>
2022-02-06 18:19:19 +0530

    reverted to module export

--- pages/locators.js
+++ pages/locators.js
@@ -1,5 +1,8 @@
-  //UI locators and city's name and url
- const accuweatherSearchBar = '//div[@class="searchbar-content"]//input[@class="search-input"]';
- const accuweatherTemperature ="//div[@class='forecast-container']//div[@class='temp']";
- const getTemperature = "//div[@class='forecast-container']//div[@class='temp']"
+module.exports = {
 
+//UI locators and city's name and url
+  accuweatherSearchBar = '//div[@class="searchbar-content"]//input[@class="search-input"]',
+ accuweatherTemperature ="//div[@class='forecast-container']//div[@class='temp']",
+  getTemperature = "//div[@class='forecast-container']//div[@class='temp']"
+
+}
